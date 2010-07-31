f6cf85dbc9b8537d5d09d448498746fb3015a338
Pedro Lamas <pedrolamas@gmail.com>
2010-07-31 11:38:10 +0000

    Initial check in

---
+++ Main/PedroLamas.WP7.AnalogClock/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace PedroLamas.WP7.AnalogClock
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var now = DateTime.Now;
+
+            double hourAngle = ((float)now.Hour) / 12 * 360 + now.Minute / 2;
+
+            double minuteAngle = ((float)now.Minute) / 60 * 360 + now.Second / 10;
+
+            double secondAngle = ((float)now.Second) / 60 * 360;
+
+            HourAnimation.From = hourAngle;
+            HourAnimation.To = hourAngle + 360;
+
+            MinuteAnimation.From = minuteAngle;
+            MinuteAnimation.To = minuteAngle + 360;
+
+            SecondAnimation.From = secondAngle;
+            SecondAnimation.To = secondAngle + 360;
+
+            ClockStoryboard.Begin();
+        }
+    }
+}
